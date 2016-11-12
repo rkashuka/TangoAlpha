bf34bb54e260f1ea06fc6bd2d7b46ff231376918
Zhuoya Chen <zhuoyachen526@gmail.com>
2016-11-12 14:26:27 -0600

    add main

--- app/src/main/java/edu/purdue/rkashuka/tangoalpha/MainActivity.java
+++ app/src/main/java/edu/purdue/rkashuka/tangoalpha/MainActivity.java
@@ -2,12 +2,41 @@ package edu.purdue.rkashuka.tangoalpha;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.ComponentName;
+import android.content.ServiceConnection;
+import android.os.IBinder;
+
 
 public class MainActivity extends AppCompatActivity {
+    ServiceConnection mTangoServiceConnection = new ServiceConnection() {
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            TangoJNINative.onTangoServiceConnected(service);
+        }
+
+        public void onServiceDisconnected(ComponentName name) {
+            // Handle this if you need to gracefully shutdown/retry
+            // in the event that Tango itself crashes/gets upgraded while running.
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        TangoInitializationHelper.bindTangoService(this, mTangoServiceConnection);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // Disconnect from Tango Service, release all the resources that the app is
+        // holding from Tango Service.
+        TangoJNINative.onPause();
+        unbindService(mTangoServiceConnection);
     }
 }

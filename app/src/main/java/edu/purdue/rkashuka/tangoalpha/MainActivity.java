084364fa4a012153b52ac7a6b0eb73ab245368f2
rkashuka <rkashuka@purdue.edu>
2016-11-12 20:10:53 -0600

    video recorder works now

--- app/src/main/java/edu/purdue/rkashuka/tangoalpha/MainActivity.java
+++ app/src/main/java/edu/purdue/rkashuka/tangoalpha/MainActivity.java
@@ -1,13 +1,27 @@
 package edu.purdue.rkashuka.tangoalpha;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.content.Context;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), recorder.class);
+                startActivity(i);
+            }
+        });
     }
+
 }

74dab06ce109ec119550d8d03893b74643dc5893
Lisa Campbell <campb215@purdue.edu>
2016-11-13 05:12:10 -0600

    OMG THIS SPEECH STUFF BETTER WORK BUT IT'S NOT PLUGGED INTO AN ACTIVITY SO IT TECHNICALLY WON'T FOR NOW BUT SHOULD WORK...IN THEORY?

---
+++ app/src/main/java/edu/purdue/rkashuka/tangoalpha/Auth.java
@@ -0,0 +1,166 @@
+package edu.purdue.rkashuka.tangoalpha;
+
+import java.io.Console;
+import java.util.Timer;
+import java.util.Timer.*;
+import org.joda.time.*;
+//import org.joda.time.DateTime;
+import java.util.concurrent.Future;
+//import java.util.concurrent.
+import com.google.common.util.concurrent.*;
+//import java.util.concurrent.CompletableFuture;
+//import java.util.concurrent.
+import com.ning.http.client.*;
+import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.*;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.*;
+
+/**
+ * Example app showing the usage of Azure Speech-to-text REST API.
+ */
+@SpringBootApplication
+public class Auth implements CommandLineRunner {
+    private static final Logger log = LoggerFactory.getLogger(Auth.class);
+
+    public static final String DATAMARKET_ACCESS_URI = "https://datamarket.accesscontrol.windows.net/v2/OAuth2-13";
+    public static final String SCOPE = "https://speech.platform.bing.com";
+
+    @Value("${TangoStuff}")
+    private String testAppId;
+
+    @Value("${8wHQvxlclRlXuNWhYnrvvNGCcITlZUxialqNgFskrDI=}")
+    private String testAppSecret;
+
+    @Value("${bing.speech.to.text.example.device.id}")
+    private String testAppDeviceId;
+
+    public static void main(String[] args) {
+        SpringApplication.run(Auth.class, args);
+    }
+
+    /**
+     * Application entry point, pass the "auth" string as an argument to force authentication against MS server.
+     * <p>
+     * Pass the path to the wav file to call the Microsoft conversion API.
+     *
+     * @param args
+     * @throws Exception
+     */
+    @Override
+    public void run(String... args) throws Exception {
+        String auth = null;
+        try {
+            auth = args[0];
+        } catch (Exception e) {
+        }
+
+        if ("auth".equals(auth)) {
+            authenticate();
+
+        } else {
+            String audioFile = args[0];
+
+            Map<String, String> c = convert(audioFile);
+
+            if (c != null)
+                log.info(c.toString());
+        }
+    }
+
+    /**
+     * Call the conversion API.
+     * <p>
+     * Check out MS documentation at:
+     * https://onedrive.live.com/view.aspx?resid=9A8C02C3B59E575!106&ithint=file%2cdocx&app=Word&authkey=!AIEJaNeh8CcDTjU
+     * <p>
+     * (you need an Azure account to acces the above file).
+     * <p>
+     * Relies on the existence of a BING_AUTH environment variable to verify the current device has been properly authenticated.
+     *
+     * @param audioFile the absolute path of the wav file to convert.
+     * @return a {@see Conversion} object encapsulating the info returned by the Microsoft API.
+     * @throws IOException
+     */
+    private HashMap<String, String> convert(String audioFile) throws IOException {
+        String auth = System.getenv("BING_AUTH");
+
+        log.info("Auth:\n" + auth);
+
+        String authToken = "Bearer " + auth;
+
+        HttpHeaders headers = new HttpHeaders();
+        List<MediaType> mediaTypes = new ArrayList<>();
+        mediaTypes.add(MediaType.TEXT_XML);
+        mediaTypes.add(MediaType.APPLICATION_JSON);
+
+        headers.setAccept(mediaTypes);
+
+        headers.add(HttpHeaders.AUTHORIZATION, authToken);
+        headers.add(HttpHeaders.HOST, "speech.platform.bing.com");
+        headers.set(HttpHeaders.CONTENT_TYPE, "audio/wav;codec=\"audio/pcm\";samplerate=8000;trustsourcerate=false");
+
+        RestTemplate restTemplate = new RestTemplate();
+        HttpEntity<byte[]> entity = new HttpEntity<>(IOUtils.toByteArray(new FileInputStream(audioFile)), headers);
+
+        String requestId = UUID.randomUUID().toString();
+
+        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl("https://speech.platform.bing.com/recognize/query")
+                .queryParam("format", "json")
+                .queryParam("appid", "D4D52672-91D7-4C74-8AD8-42B1D98141A5")
+                .queryParam("locale", "es-ES")
+                .queryParam("device.os", "Linux")
+                .queryParam("version", "3.0")
+                .queryParam("maxnbest", "3")
+                .queryParam("scenarios", "smd")
+                .queryParam("instanceid", testAppDeviceId)
+                .queryParam("requestid", requestId);
+
+        HashMap<String,String> dummy = new HashMap<>();
+
+        ResponseEntity<HashMap<String,String>> conversion =
+                restTemplate.exchange(builder.build().encode().toUri(),
+                        HttpMethod.POST, entity, (Class<HashMap<String,String>>) dummy.getClass());
+
+        return conversion.getBody();
+    }
+
+    /**
+     * Authentication helper method.
+     *
+     * @return
+     * @throws UnsupportedEncodingException
+     */
+    private ResponseEntity<AdmAccessToken> authenticate() throws UnsupportedEncodingException {
+        RestTemplate restTemplate = new RestTemplate();
+
+        String body =
+                String.format("grant_type=client_credentials&client_id=%s&client_secret=%s&scope=%s",
+                        URLEncoder.encode(testAppId, "UTF-8"),
+                        URLEncoder.encode(testAppSecret, "UTF-8"),
+                        URLEncoder.encode(SCOPE, "UTF-8"));
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+        HttpEntity<String> entity = new HttpEntity<>(body, headers);
+
+        ResponseEntity<AdmAccessToken> result =
+                restTemplate.exchange(DATAMARKET_ACCESS_URI, HttpMethod.POST, entity, AdmAccessToken.class);
+
+        log.info("export BING_AUTH=\"" + result.getBody().getAccess_token() + "\"");
+
+        return result;
+    }
+}
